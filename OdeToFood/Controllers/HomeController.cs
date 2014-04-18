b12573c9269f39aec46c79989981e9be9fb298f3
moojjoo <robertdannelly@gmail.com>
2014-04-18 16:50:45 -0400

    Ode To Code First Code Push

---
+++ OdeToFood/Controllers/HomeController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using OdeToFood.Models;
+
+namespace OdeToFood.Controllers
+{
+    public class HomeController : Controller
+    {
+        OdeToFoodDb _db = new OdeToFoodDb();
+
+        public ActionResult Index()
+        {
+            var model = _db.Restaurants.ToList();
+
+            return View(model);
+        }
+
+        public ActionResult About()
+        {
+            var model = new AboutModel {Name = "Robert", Location = "Mooresville, NC USA"};
+
+            return View(model);
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        protected override void Dispose(bool disposing)  ///Cleans up resources instead of garbage collector
+        {
+            if(_db != null)
+            {
+                      _db.Dispose();
+            }
+        base.Dispose(disposing);
+        }
+
+    }
+}
---
+++ OdeToFood/Models/Restaurant.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.AccessControl;
+using System.Web;
+using DotNetOpenAuth.OpenId.Extensions.AttributeExchange;
+
+namespace OdeToFood.Models
+{
+    public class Restaurant
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string City { get; set; }
+        public string Country { get; set; }
+        public ICollection<RestauranReview> Reviews { get; set; }
+    }
+}
